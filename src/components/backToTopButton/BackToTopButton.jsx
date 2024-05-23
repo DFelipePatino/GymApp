270ff19da4306dba98cb9f4dfe7afe22781aca5b
Daniel Patino <dafepa10c29@gmail.com>
2024-05-22 20:33:23 -0500

    card item implemented

--- src/components/backToTopButton/BackToTopButton.jsx
+++ src/components/backToTopButton/BackToTopButton.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './backToTopButton.css'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 
 function BackToTopButton() {
     return (
         <div>
-            <KeyboardArrowUpIcon
+            <KeyboardDoubleArrowUpIcon
                 className='backToTop'
                 onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
             />
-            <p className='backToTop2'>Top</p>
+            {/* <p className='backToTop2'>Top</p> */}
         </div>
     )
 }
